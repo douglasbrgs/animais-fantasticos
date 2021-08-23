c59dc085041c47b428054b44622b2991301d956b
Douglas Borges <douglasborges19@gmail.com>
2021-08-23 17:42:55 -0300

    remove pasta main.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
   output: {
-    path: path.resolve(__dirname, './main.js'),
+    path: path.resolve(__dirname, './'),
     filename: 'main.js',
   },
 };
