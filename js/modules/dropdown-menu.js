f6b74572a0f8cf11bbb91fcfb42e7235dfa81ebb
Douglas Borges <douglasborges19@gmail.com>
2021-09-21 10:09:52 -0300

    transforma dropdown-menu em classe

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,20 +1,45 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+    this.activeClass = 'ativo';
 
-  function handleClick(event) {
+    // define eventos a serem observados fora do menu
+    if (events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
+
+    // bind do callback a classe
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  // adiciona classe para ativar o menu
+  // além de um observador para eventos fora dele
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add('ativo');
 
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('ativo');
+    const element = event.currentTarget;
+
+    element.classList.add(this.activeClass);
+
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  // adiciona evento a cada um dos menus
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import ScrollAnima from './modules/scroll-anima.js';
@@ -28,7 +28,9 @@ tooltip.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
-initDropdownMenu();
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
+
 initMenuMobile();
 initFuncionamento();
 
