0282b626b6cf31f5b54609ff8e362a4ef9512b52
Douglas Borges <douglasborges19@gmail.com>
2021-09-21 13:59:16 -0300

    transforma funcionamento em classe

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,17 +1,39 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
-  const semana = funcionamento.dataset.semana.split(',').map(Number);
-  const horario = funcionamento.dataset.horario.split(',').map(Number);
+  dadosFuncionamento() {
+    this.semana = this.funcionamento.dataset.semana.split(',').map(Number);
+    this.horario = this.funcionamento.dataset.horario.split(',').map(Number);
+  }
 
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horaAgora = dataAgora.getHours();
+  tempoAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horaAgora = this.dataAgora.getUTCHours() - 3; // horario de Brasilia
+  }
 
-  const semanaAberto = semana.indexOf(diaAgora) !== -1;
-  const horarioAberto = horaAgora >= horario[0] && horaAgora < horario[1];
+  estaAberto() {
+    this.semanaAberto = this.semana.indexOf(this.diaAgora) !== -1;
+    this.horarioAberto = this.horaAgora >= this.horario[0] && this.horaAgora < this.horario[1];
 
-  if (semanaAberto && horarioAberto) {
-    funcionamento.classList.add('ativo');
+    return this.semanaAberto && this.horarioAberto;
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.tempoAgora();
+      this.ativaAberto();
+    }
+    return this;
   }
 }
